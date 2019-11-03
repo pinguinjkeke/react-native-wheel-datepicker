53ff8279d1ff5a88b1d9c751a35208be717d21c8
davidgovea <govea.d@gmail.com>
2019-11-03 10:21:54 -0800

    feat: add typescript definition (#9)

---
+++ src/index.d.ts
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import * as ReactNative from 'react-native';
+
+interface PickerProps {
+  textColor?: string;
+  textSize?: number;
+  itemSpace?: number;
+  itemStyle?: ReactNative.TextProps;
+  onValueChange: (val: any) => void;
+  pickerData: any[];
+  style: ReactNative.ViewProps;
+  selectedValue: any;
+}
+
+interface DatePickerBaseProps {
+  date?: Date;
+  maximumDate?: Date;
+  minimumDate?: Date;
+  mode?: 'date' | 'time' | 'datetime';
+  onDateChange: (date: Date) => void;
+  minuteInterval?: number; // todo: further restrict
+}
+
+interface DatePickerAndroidProps {
+  labelUnit?: {
+    year?: string;
+    month?: string;
+    date?: string;
+  };
+  order?: string;
+  use12Hours?: boolean;
+  style?: ReactNative.ViewProps;
+  textColor?: string;
+  textSize?: number;
+  itemSpace?: number;
+}
+
+export const registerCustomDatePickerIOS: (
+  CustomDatePickerIOS: React.Component
+) => React.Component;
+
+export class Picker extends React.Component<PickerProps> {}
+
+export class DatePicker extends React.Component<
+  DatePickerBaseProps & DatePickerAndroidProps
+> {}
