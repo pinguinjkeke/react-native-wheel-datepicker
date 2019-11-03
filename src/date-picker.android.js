9f88863da1e0a17c757a87ace34da64c211a192a
David Govea <govea.d@gmail.com>
2019-11-02 19:42:58 -0700

    fix: oneOf proptypes syntax error

--- src/date-picker.android.js
+++ src/date-picker.android.js
@@ -35,7 +35,7 @@ export default class DatePicker extends PureComponent {
     minimumDate: PropTypes.instanceOf(Date),
     mode: PropTypes.oneOf(['date', 'time', 'datetime']),
     onDateChange: PropTypes.func.isRequired,
-    minuteInterval: PropTypes.oneOf(1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30),
+    minuteInterval: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30]),
     style: ViewPropTypes.style,
     textColor: ColorPropType,
     textSize: PropTypes.number,
